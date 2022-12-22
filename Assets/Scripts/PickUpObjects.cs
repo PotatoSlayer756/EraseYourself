0c4812439ef468134e10c51c187d2f3b0e98cd34
Alexander Baydalinov <64281569+Rezenkron@users.noreply.github.com>
2022-12-22 19:13:22 +0300

    Better pick up, portals, bugs fixes

--- Assets/Scripts/PickUpObjects.cs
+++ Assets/Scripts/PickUpObjects.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using TMPro;
 
 public class PickUpObjects : MonoBehaviour
 {
@@ -6,47 +7,92 @@ public class PickUpObjects : MonoBehaviour
     [SerializeField] private Transform handsPosition;
     [SerializeField] private float maxDistance;
     [SerializeField] private Camera cam;
-    [SerializeField] GameObject rmbIcon;
-    bool isPicked = false;
+
+    [SerializeField] private GameObject rmbIcon;
+    [SerializeField] private GameObject absorbTip;
+    [SerializeField] private TextMeshProUGUI absorbedPixelsText;
+
+    private GameObject portal;
+
+    private GameObject[] allPixelsOnLevel;
+    private int pixelsOnLevelAmount;
+
+    private int absorbedPixels = 0;
+    private bool isPicked = false;
+    private Rigidbody rb;
+    private void Start()
+    {
+        allPixelsOnLevel = GameObject.FindGameObjectsWithTag("BrokenPixel");
+        pixelsOnLevelAmount = allPixelsOnLevel.Length;
+        portal = GameObject.FindGameObjectWithTag("Portal");
+        portal.SetActive(false);
+    }
+
     GameObject pickedObject;
     private void Update()
     {
         RaycastHit hit;
 
-
         if (Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, maxDistance, pickupableLayer))
         {
-            if(!isPicked) rmbIcon.SetActive(true);
+            if (!isPicked) rmbIcon.SetActive(true);
 
             if (Input.GetButtonDown("Fire2") && !isPicked)
             {
-                if (hit.transform.gameObject.tag == "BrokenPixel")
-                {
-                    hit.transform.gameObject.GetComponentInChildren<Animator>().SetBool("isPicked", true);
-                }
                 isPicked = true;
                 pickedObject = hit.transform.gameObject;
                 pickedObject.transform.position = handsPosition.position;
                 pickedObject.transform.SetParent(handsPosition);
+
+                if (pickedObject.gameObject.tag == "BrokenPixel")
+                {
+                    pickedObject.gameObject.GetComponentInChildren<Animator>().SetBool("isPicked", isPicked);
+                    absorbTip.SetActive(true);
+                }
+
+                if (pickedObject.GetComponent<Rigidbody>() != null)
+                {
+                    pickedObject.GetComponent<Rigidbody>().isKinematic = true;
+                }
             }
             else if (Input.GetButtonDown("Fire2") && isPicked)
             {
-                if (hit.transform.gameObject.tag == "BrokenPixel")
+                if (pickedObject.gameObject.tag == "BrokenPixel")
                 {
-                    hit.transform.gameObject.GetComponentInChildren<Animator>().SetBool("isPicked", false);
+                    pickedObject.gameObject.GetComponentInChildren<Animator>().SetBool("isPicked", !isPicked);
+                    absorbTip.SetActive(false);
                 }
+
+                if(pickedObject.GetComponent<Rigidbody>() != null)
+                {
+                    pickedObject.GetComponent<Rigidbody>().isKinematic = false;
+                }
+
                 isPicked = false;
                 pickedObject.transform.SetParent(null);
                 pickedObject = null;
-                
             }
-            
-        }    
+        }
         else
         {
             rmbIcon.SetActive(false);
         }
 
+        if (pickedObject != null && pickedObject.gameObject.tag == "BrokenPixel" && Input.GetButtonDown("Fire1"))
+        {
+            absorbTip.SetActive(false);
+            absorbedPixels++;
+            Destroy(pickedObject);
+            pickedObject = null;
+            isPicked = false;
+        }
+
+        absorbedPixelsText.text = $"{absorbedPixels}";
+
+        if(absorbedPixels == pixelsOnLevelAmount)
+        {
+            portal.SetActive(true);
+        }
     }
 
 }
